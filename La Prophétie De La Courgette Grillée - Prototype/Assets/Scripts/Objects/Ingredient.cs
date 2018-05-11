b5b4ca416ef86aa17b811f584bb7467b811a6382
Rutabaga <mrrutabaga@hotmail.com>
2018-05-11 17:49:46 +0200

    Création ingredients + rajout map

---
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Objects/Ingredient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+	public new string name;
+	[TextArea] public string description;
+
+	public int quality;
+	public int cost;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "player")
+		{
+			// Add ingredient to inventory
+			Destroy(gameObject);
+		}
+	}
+}
