ee5d3bbe9e3495562d502f4a99e1ac224f139364
MrRutabaga <MrRutabaga@hotmail.com>
2018-10-09 22:30:53 +0200

    Argent

---
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Money/Money.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public int amount;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "player")
+        {
+            PickUp();
+        }
+    }
+
+    public void PickUp()
+    {
+        FindObjectOfType<MoneyManager>().addMoney(amount);
+        Destroy(this.gameObject);
+    }
+}
---
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Money/MoneyManager.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public int currentMoney;
+
+    public TextMeshProUGUI moneyTextUI;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void addMoney(int dif)
+    {
+        currentMoney += dif;
+        if (currentMoney < 0)
+            currentMoney = 0;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        moneyTextUI.text = "" + currentMoney;
+    }
+}
