990659e64eac2fe4e8224d566c6416000b9d2c15
moipaslui <moi.pas.lui45@gmail.com>
2018-11-24 16:43:15 +0100

    Confection du menu

---
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitMenu()
+    {
+        Application.Quit();
+    }
+}
