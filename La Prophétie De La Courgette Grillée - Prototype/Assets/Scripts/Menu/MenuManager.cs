8ae16a94b1823305e78e81d730a1e022c275997c
moipaslui <moi.pas.lui45@gmail.com>
2018-12-06 17:14:57 +0100

    Options dans le menu titre

--- La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Menu/MenuManager.cs
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Menu/MenuManager.cs
@@ -1,12 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject options;
+    public AudioSource source;
+    public Text text;
+
+    private void Update()
+    {
+        double volume = System.Math.Round((double)source.volume,2);
+        text.text = volume.ToString();
+    }
+
     public void LoadScene(int scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void AfficherOptions()
+    {
+        options.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void RetournerMenu()
+    {
+        menu.SetActive(false);
+        options.SetActive(true);
     }
 
     public void QuitMenu()
---
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource source;
+
+	void Start()
+    {
+        source = GetComponent<AudioSource>();
+        DontDestroyOnLoad(source);
+	}
+
+    public void AugmenterVolume()
+    {
+        source.volume = source.volume + 0.02f;
+    }
+
+    public void BaisserVolume()
+    {
+        source.volume = source.volume - 0.02f;
+    }
+}
