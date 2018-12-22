7de43f3ef01e781f4faf9bcf225691afc282ca3f
MrRutabaga <mrrutabaga@hotmail.com>
2018-12-22 17:29:58 +0100

    Ajout journal de quete

---
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Quests/QuestLog.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class QuestLog : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject panel;
+
+    private QuestSlot[] questSlots;
+
+    void Start()
+    {
+        title.SetActive(false);
+        panel.SetActive(false);
+
+        questSlots = panel.GetComponentsInChildren<QuestSlot>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Quest"))
+        {
+            if(panel.activeSelf)
+            {
+                panel.SetActive(false);
+                title.SetActive(false);
+            }
+            else
+            {
+                panel.SetActive(true);
+                title.SetActive(true);
+                UpdateUI();
+            }
+        }
+    }
+
+    private void UpdateUI()
+    {
+        int i = 0;
+        foreach (Quest quest in GameManager.questManager.quests)
+        {
+            if(quest.isActive)
+            {
+                if (i < 3)
+                {
+                    questSlots[i].gameObject.SetActive(true);
+                    questSlots[i].quest = quest.questData;
+                    questSlots[i].UpdateUI();
+                    i++;
+                }
+            }
+        }
+        for(; i < 3; i++)
+        {
+            questSlots[i].gameObject.SetActive(false);
+        }
+    }
+}
---
+++ La Prophétie De La Courgette Grillée - Prototype/Assets/Scripts/Quests/QuestSlot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class QuestSlot : MonoBehaviour
+{
+    public QuestData quest;
+
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI description;
+
+    public void UpdateUI()
+    {
+        title.text = quest.title;
+        description.text = quest.description;
+    }
+}
